43bf2372c58c02082be65dd3a7f317b65124f104
kevin sujono <kevinsujono2@gmail.com>
2025-07-15 18:17:21 +0700

    Commit New Update

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,11 +1,21 @@
 package main
 
+import "github.com/Razor4456/FoundationBackEnd/internal/store"
+
 type ApplicationApi struct {
-	Config Config
+	Config   Config
+	Function store.Function
 }
 
 type Config struct {
+	Addr string
+	Db   Dbconfig
+	Env  string
 }
 
 type Dbconfig struct {
+	Addr         string
+	MaxOpenConns int
+	MaxIdleConss int
+	MaxIdleTime  string
 }
