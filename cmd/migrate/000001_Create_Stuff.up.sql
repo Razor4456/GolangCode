ed117ebf4fc951c41fef0d2e71346a7bc1da61d8
kevin sujono <kevinsujono2@gmail.com>
2025-07-16 11:41:55 +0700

    New Commit

---
+++ cmd/migrate/000001_Create_Stuff.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS stuff(
+    id INT PRIMARY KEY,
+    nama_barang varchar (200) NOT NULL,
+    jumlah_barang INT NOT NULL,
+    harga INT NOT NULL,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+);
--- internal/store/stuff.go
+++ internal/store/stuff.go
@@ -8,6 +8,11 @@ import (
 )
 
 type PostStuff struct {
+	Id           int64  `json:"id"`
+	Namabarang   string `json:"nama_barang"`
+	Jumlahbarang int64  `json:"jumlah_barang"`
+	Harga        int64  `json:"harga"`
+	CreatedAt    string `json:"created_at"`
 }
 
 type StuffApi struct {
